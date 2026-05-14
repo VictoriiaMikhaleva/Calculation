3c6f1807c7eb3542b5d7ec37da0182e316917c1f
VictoriiaMikhaleva <victoriaskach@gmail.com>
2026-05-14 11:32:13 +0300

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,5 @@ import react from "@vitejs/plugin-react";
 
 export default defineConfig({
   plugins: [react()],
-  // Иначе .JPG (верхний регистр) попадает в JS-парсер и падает import analysis
   assetsInclude: ["**/*.JPG", "**/*.JPEG", "**/*.PNG", "**/*.GIF", "**/*.WEBP"],
 });
